ddf628846f5929a4731b3785250257df75fc2709
Adam Cooper <adam.cooper@nottingham.ac.uk>
2018-06-07 13:20:32 +0100

    Update index with correct response code for the root domain

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ app.use(express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-  res.status(410);
+  res.status(200);
   res.render('index');
 })
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port ' + process.env.PORT + '!')
+  console.log('App listening on port ' + process.env.PORT + '!')
 })
