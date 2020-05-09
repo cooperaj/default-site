479acb623702e51fd1432954a3a980322dfe2d99
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-05-09 14:32:16 +0100

    Add purge css and set it up

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -6,11 +6,13 @@ const cleancss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const purgecss = require('gulp-purgecss');
  
 exports.compileSass = function compileSass() {
     return src('assets/stylesheets/main.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(purgecss({content: ['views/**/*.pug']}))
         .pipe(autoprefixer())
         .pipe(cleancss())
         .pipe(rename({
