8ff77f41fb1d67f5fced332f11d8d22a7a670231
Adam Cooper <adam@acpr.dev>
2026-04-01 15:36:50 +0100

    Add cors header to redirect

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ app.use(express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/.well-known/webfinger', function (req, res) {
+  res.appendHeader('Access-Control-Allow-Origin', '*')
   res.redirect(301, 'https://social.n8e.dev' + req.originalUrl)
 })
 
 app.get('/.well-known/host-meta', function (req, res) {
+  res.appendHeader('Access-Control-Allow-Origin', '*')
   res.redirect(301, 'https://social.n8e.dev' + req.originalUrl)
 })
 
 app.get('/.well-known/nodeinfo', function (req, res) {
+  res.appendHeader('Access-Control-Allow-Origin', '*')
   res.redirect(301, 'https://social.n8e.dev' + req.originalUrl)
 })
 
